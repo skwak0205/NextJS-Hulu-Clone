061d12decc162fc81071172f91163a01af1794e8
Soojung Kwak <gsj2464@gmail.com>
2021-06-25 17:24:47 +0900

    css, flip move 추가

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,15 @@
+import { forwardRef } from 'react';
 import Image from 'next/image';
 import { ThumbUpIcon } from '@heroicons/react/outline';
 
-const Thumbnail = ({ result }) => {
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = 'https://image.tmdb.org/t/p/original/';
 
   return (
-    <div className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'>
+    <div
+      ref={ref}
+      className='p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50'
+    >
       <Image
         src={
           `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
@@ -31,6 +35,6 @@ const Thumbnail = ({ result }) => {
       </div>
     </div>
   );
-};
+});
 
 export default Thumbnail;
