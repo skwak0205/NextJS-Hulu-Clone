815f4897d4018b2f8c6e23f93ed2362b96d737e3
Soojung Kwak <gsj2464@gmail.com>
2021-06-25 16:03:51 +0900

    thumbnail component 추가

--- components/Cards.js
+++ components/Cards.js
@@ -1,5 +1,13 @@
+import Thumbnail from './thumbnail';
+
 const Cards = ({ results }) => {
-  return <div></div>;
+  return (
+    <div>
+      {results.map((result) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </div>
+  );
 };
 
 export default Cards;
