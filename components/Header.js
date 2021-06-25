bf679c6e9a3190caf868e639a1d028663ebf68f5
Soojung Kwak <gsj2464@gmail.com>
2021-06-25 14:17:21 +0900

    header item 추가

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,25 @@
+import HeaderItem from './HeaderItem';
 import Image from 'next/image';
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline';
 
 const Header = () => {
   return (
-    <header className=''>
+    <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+      <div className='flex flex-grow justify-evenly max-w-2xl'>
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+      </div>
       <Image
         className='object-contain'
         src='http://links.papareact.com/ua6'
